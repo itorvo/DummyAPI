5023c53d156653a91c0ec5500e4a0f5cf91882b7
Raul Vergara Ortiz <raul.vergara@pragma.com.co>
2021-04-12 12:16:25 -0500

    Add Files

---
+++ DummyAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DummyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            string name = Environment.GetEnvironmentVariable("Nombre");
+            string greetings = $"Hola {name}";
+            return Ok(greetings);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DummyAPI
 Test Docker
+
+## Create Images Docker
+`docker build -t "apitest" -f DummyAPI\Dockerfile .`
+
+## Run Container
+`docker run -it -p 8000:80 -e Nombre=RaulDocker apitest:latest`
+
+## Assign tag to image
+`docker tag apitest [NumberAccountAWS].dkr.ecr.us-east-1.amazonaws.com/testapi:latest`
+
+## Login Docker with AWS
+`Invoke-Expression (aws ecr get-login --no-include-email)`
+
+## Push images in ECR
+`docker push [NumberAccountAWS].dkr.ecr.us-east-1.amazonaws.com/testapi:latest`
